f953f58cf5166204115f84031f6cbca9b550c146
RahelMoges <rahelmoges192@gmail.com>
2022-09-09 07:08:53 -0700

    capita vs small

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)  
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
-put char(c);
-put char('\n');
+putchar(c);
+putchar('\n');
 return (0);
 }
 
