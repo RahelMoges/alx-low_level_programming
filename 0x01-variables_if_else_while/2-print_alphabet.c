46b3795da4f8204303563118a7bb210e1fccd563
RahelMoges <rahelmoges192@gmail.com>
2022-09-09 06:41:00 -0700

    capita vs small

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-\\**
-*main - Entry point
-*Description: prints the alphabet in lowercase\n
-*Return: Always 0 (success)
-*/
 int main(void)
 {
 int ch;
 for (ch = 'a' ; ch <= 'z'; ch++)
 {
-putchar(ch);
+put char(ch);
 }
-putchar('\n');
+put char('\n');
 return (0);
 }
      
