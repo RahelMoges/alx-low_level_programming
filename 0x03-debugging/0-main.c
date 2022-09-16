e98dc870abec076016ecd283198cffbf161c5409
RahelMoges <rahelmoges192@gmail.com>
2022-09-16 11:19:23 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-  
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -11,6 +11,6 @@ int i;
 
 i = 0;
 positive_or_negative(i);
- 
+
 return (0);
 }
