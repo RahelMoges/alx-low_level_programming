3de4d3c148629eb8b6904e5c3e7a36f7254b4c37
RahelMoges <rahelmoges192@gmail.com>
2022-09-19 06:19:45 -0700

    arrray

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,7 +9,6 @@
 
 int main(void)
 {
-  
 int n;
 n = 402;
 printf("n=%d\n", n);
@@ -17,4 +16,3 @@ reset_to_98(&n);
 printf("n=%d\n", n);
 return (0);
 }
-
