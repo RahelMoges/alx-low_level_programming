9323f9edef05f4d19463fe512fde9ffb74f8e53e
RahelMoges <rahelmoges192@gmail.com>
2022-09-23 23:27:36 -0700

    more pointers arays

---
+++ 0x04-more_functions_nested_loops/2-main.h
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+
+* _isupper - a function that checks for uppercase character
+* @ digit 0-9
+* @c: input
+* Return: if c is uppercase, 0 otherwise
+*/
+
+int  _isupper(int c)
+{
+if ((c >= 'A') && (c <= '2'))
+return (1)
+else
+return(0)
+}
