fbee42a383aa45119ccbb5fb163562dc96b31514
RahelMoges <rahelmoges192@gmail.com>
2022-10-06 07:45:12 -0700

    tasks

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
-#ifndef main_h
+#ifndef MAIN_H
 
-#define holberton_h
+#define MAIN_H
+
+
 
 int _putchar(char c);
 
@@ -11,6 +13,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 
 int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+void errors(void);
+
+int _strlen(char *s);
+
+int is_digit(char *s);
 
 
 
