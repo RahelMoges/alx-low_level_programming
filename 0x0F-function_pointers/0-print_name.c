7ad8254c03d46d170c4aac07a2af6710dd402bfc
RahelMoges <rahelmoges192@gmail.com>
2022-10-13 11:57:37 -0700

    fpointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
 
 /**
-* print_name - Prints a name
-* @name: The name to prints
-* @f: Pointer to function
-*
-* Return: Nothing
-*/
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-f(name);
+	if (name && f)
+	{
+		f(name);
+	}
 }
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,1 @@
-function pointer
+Pointer functions
