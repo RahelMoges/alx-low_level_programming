7911ad3ac43792c22d791f1d1478449fd4e061fe
RahelMoges <rahelmoges192@gmail.com>
2022-12-12 11:04:04 -0800

    push

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,12 +1,7 @@
-fndef _SHARED_LIB_H_
-
+#ifndef _SHARED_LIB_H_
 #define _SHARED_LIB_H_
 
-
-
 #include <stdlib.h>
-
-
 
 int _putchar(char c);
 
@@ -47,7 +42,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
-
-
 
 #endif
